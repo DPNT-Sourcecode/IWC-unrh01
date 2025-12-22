043a83d748b9dda58bc3da5a8ca8d53dd5ad622a
sbx_user1051 <sbx_user1051@169.254.5.201>
2025-12-22 19:15:06 +0000

    Mon Dec 22 19:15:06 UTC 2025

--- test/solution_tests/IWC/test_queue_solution.py
+++ test/solution_tests/IWC/test_queue_solution.py
@@ -4,8 +4,20 @@ from .utils import call_dequeue, call_enqueue, call_size, iso_ts, run_queue
 
 
 def test_enqueue_size_dequeue_flow() -> None:
-    run_queue([
-        call_enqueue("companies_house", 1, iso_ts(delta_minutes=0)).expect(1),
-        call_size().expect(1),
-        call_dequeue().expect("companies_house", 1),
-    ])
+    run_queue(
+        [
+            call_enqueue("companies_house", 1, iso_ts(delta_minutes=0)).expect(1),
+            call_size().expect(1),
+            call_dequeue().expect("companies_house", 1),
+        ]
+    )
+
+
+def test_enqueue_rule_of_three() -> None:
+    run_queue(
+        [
+            call_enqueue("companies_house", 1, iso_ts(delta_minutes=3)).expect(1),
+            call_enqueue("bank_statements", 2, iso_ts(delta_minutes=3)).expect(1),
+        ]
+    )
+
