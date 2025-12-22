25c654ab0e85e9f6173a17ced1c320919e1b9c3b
sbx_user1051 <sbx_user1051@169.254.5.201>
2025-12-22 19:18:06 +0000

    Mon Dec 22 19:18:06 UTC 2025

--- test/solution_tests/IWC/test_queue_solution.py
+++ test/solution_tests/IWC/test_queue_solution.py
@@ -17,7 +17,11 @@ def test_enqueue_rule_of_three() -> None:
     run_queue(
         [
             call_enqueue("companies_house", 1, iso_ts(delta_minutes=3)).expect(1),
-            call_enqueue("bank_statements", 2, iso_ts(delta_minutes=3)).expect(1),
+            call_enqueue("bank_statements", 2, iso_ts(delta_minutes=3)).expect(2),
+            call_enqueue("id_verification", 1, iso_ts(delta_minutes=3)).expect(3),
+            call_enqueue("something_else", 1, iso_ts(delta_minutes=3)).expect(4),
+            call_enqueue("id_verification", 2, iso_ts(delta_minutes=3)).expect(5),
         ]
     )
+
 
