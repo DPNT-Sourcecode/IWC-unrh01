21934d7431513eb4e544e57a8512b9ed6eda5e7b
sbx_user1051 <sbx_user1051@169.254.2.213>
2025-12-30 12:13:59 +0000

    Tue Dec 30 12:13:59 UTC 2025

--- lib/solutions/IWC/queue_solution_legacy.py
+++ lib/solutions/IWC/queue_solution_legacy.py
@@ -184,6 +184,12 @@ class Queue:
 
                         if task_age >= 300:
                             metadata["priority"] = Priority.NORMAL
+                            task_dt = datetime.fromisoformat(task.timestamp).replace(
+                                tzinfo=None
+                            )
+                            metadata["group_earliest_timestamp"] = str(
+                                task_dt - timedelta(seconds=1)
+                            )
                         else:
                             metadata["priority"] = Priority.LOW
                     else:
@@ -315,3 +321,4 @@ async def queue_worker():
         logger.info(f"Finished task: {task}")
 ```
 """
+
